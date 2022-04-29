a92e490155e58a62cee3dac8496b04311b074e89
grimxreaper <hizahrazadeh@gmail.com>
2022-04-29 17:50:04 -0400

    add modal toggle

--- modal.js
+++ modal.js
@@ -1,1 +1,17 @@
-const portfolioContainer = document.querySelector('.service_block.counseling');
+const counselingBlock = document.querySelector('.service_block_counseling');
+
+if (counselingBlock) {
+    counselingBlock.addEventListener('click', e => {
+        // console.log(e)
+        e.preventDefault()
+
+        const modalToggle = e.target.closest('.cta_counseling')
+        console.log(modalToggle)
+
+        if (!modalToggle) return
+//target parent and then the parent's sibling
+        const modal = modalToggle.parentNode.nextElementSibling
+
+    })
+}
+
