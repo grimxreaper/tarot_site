73507b29dd0190cc9b855411e6a511eee98822b5
grimxreaper <hizahrazadeh@gmail.com>
2022-04-29 17:59:32 -0400

    close modal

--- modal.js
+++ modal.js
@@ -1,19 +1,24 @@
-const counselingBlock = document.querySelector('.service_block_counseling');
+const counselingBlock = document.querySelector(".service_block_counseling");
 
 if (counselingBlock) {
-    counselingBlock.addEventListener('click', e => {
-        // console.log(e)
-        e.preventDefault()
+  counselingBlock.addEventListener("click", (e) => {
+    // console.log(e)
+    e.preventDefault();
 
-        const modalToggle = e.target.closest('.cta_counseling')
-        console.log(modalToggle)
+    const modalToggle = e.target.closest(".cta_counseling");
+    console.log(modalToggle);
 
-        if (!modalToggle) return
-//target parent and then the parent's sibling
-        const modal = modalToggle.parentNode.nextElementSibling
+    if (!modalToggle) return;
+    //target parent and then the parent's sibling
+    const modal = modalToggle.parentNode.nextElementSibling;
+    const closeButton = modal.querySelector(".close_button");
+    //opens modal
+    modal.classList.add("is-open");
+    //closes modal
+    closeButton.addEventListener("click", (_) => {
+      modal.classList.remove("is-open");
+    });
 
-        modal.classList.add('is-open')
-
-    })
+    //
+  });
 }
-
